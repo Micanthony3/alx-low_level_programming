a420d3d609af68b2ba16a57cf7597e034b6d60c4
Michael Anthony <michaelanthony9393@gmail.com>
2022-12-12 20:04:10 -0800

    jk

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,5 @@
 #include <unistd.h>
 
-
-
 /**
  *
  *  * main - Entry Point
@@ -16,14 +14,11 @@
  *
  *       */
 
-
-
 int main(void)
 
 {
 
 		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-			return (1);
-
+return (1);
 }
