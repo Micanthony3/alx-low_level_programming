a88635f2beb0494bb64c2dc1d08ae3d3832b5e61
Michael <michaelanthony9393@gmail.com>
2022-12-29 12:25:39 -0800

    pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "kasiye.h"
+#include <stdio.h>
+
+/**
+ *  reset_to_98 - take pointer to an int parameter and updates
+ *  the value it points to be 98
+ *  @n: point to an int
+ *  Return: Nothing
+ */
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
