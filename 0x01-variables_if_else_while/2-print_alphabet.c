6d5882158b42c4825311f58f5ebbabd2988005f4
Michael <michaelanthony9393@gmail.com>
2022-12-29 19:29:45 -0800

    excel

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,7 +3,7 @@
 /**
  * main -Entry point
  *
- *    * Return: Always 0 (Success)
+ *Return: Always 0 (Success)
 */
 int main(void)
 {
@@ -11,7 +11,7 @@ int main(void)
 c = 'a';
 	while
 
-		(c < = 'z')
+		(c <= 'z')
 		{
 			putchar(c);
 			c++;
