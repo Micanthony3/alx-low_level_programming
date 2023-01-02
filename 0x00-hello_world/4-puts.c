710993238c829915c472758e3d394fbab2e83859
Michael <michaelanthony9393@gmail.com>
2023-01-02 03:59:54 -0800

    taks 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - Entry
- * Return; Always 0
+ * main - Entry point
+ *
+ * Description: prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
