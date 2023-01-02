fc349b55f1bee4e5ce54cadb963725fc7918c785
Michael <michaelanthony9393@gmail.com>
2023-01-02 03:38:38 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
 #include "main.h"
+
 /**
- * main - print _putchar
- * is written by Micky
- * Return:0
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	_putchar(' ');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -16,5 +16,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
