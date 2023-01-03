8ac3329f20467b25186c33e2b8b8813e7a1a3619
Michael <michaelanthony9393@gmail.com>
2023-01-03 12:57:42 -0800

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,11 @@
-#include "kasiye.h"
+#include "main.h"
 
 /**
- *  reset_to_98 - take pointer to an int parameter and updates
- *  the value it points to be 98
- *  @n: point to an int
- *  Return: Nothing
+ * reset_to_98 - main
+ * @n: - pinter to int
  */
-void reset_to_98(int *n);
+
+void reset_to_98(int *n)
 {
-	*n = 98;
+*n = 98;
 }
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,1 +1,1 @@
-this is my array project
+0x05. C - Pointers, arrays and strings
